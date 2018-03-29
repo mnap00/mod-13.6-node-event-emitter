61ad0a0f94e16b8fb1349fc6aab96a5119458804
mnap <learn@10g.pl>
2018-03-29 21:41:27 +0200

    Add switch statement

--- program.js
+++ program.js
@@ -4,11 +4,20 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
-            process.stdout.write('Wrong instruction!\n');
+        switch (instruction) {
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+            case '/ver':
+                var version = process.versions.node;
+                process.stdout.write(version + '\n');
+                break;
+            case '/lang':
+                var language = process.env.LANG;
+                process.stdout.write(language + '\n');
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
         }
     }
 });
