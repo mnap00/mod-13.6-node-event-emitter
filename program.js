88dbdfb2929c09fa1e52efec54909861a6b5f64f
mnap <learn@10g.pl>
2018-03-29 21:24:07 +0200

    Add base script - if else loop

---
+++ program.js
@@ -0,0 +1,14 @@
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        if (instruction === '/exit') {
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+        } else {
+            process.stdout.write('Wrong instruction!\n');
+        }
+    }
+});
