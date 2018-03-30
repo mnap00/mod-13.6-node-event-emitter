0055d3d75b53b0aac01f5e996ab2baf021a0e6df
mnap <learn@10g.pl>
2018-03-30 23:09:30 +0200

    Add colors npm module

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 var time = require('./timeformat');
 
 function getOSinfo() {
@@ -12,12 +13,12 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', time.timeformat(uptime));
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log('System:'.black.bgGreen, type);
+    console.log('Release:'.red, release);
+    console.log('CPU model:'.magenta, cpu);
+    console.log('Uptime: ~'.green, time.timeformat(uptime));
+    console.log('User name:'.yellow, userInfo.username);
+    console.log('Home dir:'.white, userInfo.homedir);
 }
 
 exports.print = getOSinfo;
