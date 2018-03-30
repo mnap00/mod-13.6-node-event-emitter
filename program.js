ddbac9d3aff932e0673c6964d2f0d192c116b830
mnap <learn@10g.pl>
2018-03-30 21:19:58 +0200

    Move getOSinfo function to separate module

--- program.js
+++ program.js
@@ -1,23 +1,4 @@
-var os = require('os');
-
-function getOSinfo() {
-    var type = os.type();
-    var release = os.release();
-    var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
-    var userInfo = os.userInfo();
-    if (type === 'Darwin') {
-        type = 'OSX';
-    } else if (type === 'Windows_NT') {
-        type = 'Windows';
-    }
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
-}
+var OSinfo = require('./OSinfo');
 
 process.stdin.setEncoding('utf-8');
 
@@ -42,7 +23,7 @@ process.stdin.on('readable', function() {
                 process.stdout.write('hello!\n');
                 break;
             case '/getOSinfo':
-                getOSinfo();
+                OSinfo.print();
                 break;
             default:
                 process.stderr.write('Wrong instruction!\n');
